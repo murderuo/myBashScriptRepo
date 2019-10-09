0f67c57131e7494b838b99a7b297040f8da5fce7
OkUgur <murderuo@users.noreply.github.com>
2019-10-09 11:45:21 +0300

    ilk bash uygulaması

---
+++ ilkuygulama.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+echo "programın ismi  : $0"
+echo "birinci argüman : $1"
+echo "ikinci argüman : $2"
+echo "üçüncü argüman : $3"
+
+echo "tüm argümanlar : $*"
+echo "argüman sayısı : $#"
+
+shift
+echo " "
+#echo "programın ismi \: $0"
+echo "birinci argüman : $1"
+echo "ikinci argüman  : $2"
+echo "üçüncü argüman  : $3"
+
+echo "tüm argümanlar : $*"
+echo "argüman sayısı : $#"
+
+
